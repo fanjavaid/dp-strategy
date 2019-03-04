607cfcc6375bb9a1b9be5a1081bb18a4d4cafeff
Fandi Akhmad <fanjavaid@gmail.com>
2019-03-04 11:32:24 +0700

    Simplify code

--- src/com/fanjavaid/strategy_pattern/java/example01/PaymentClient.java
+++ src/com/fanjavaid/strategy_pattern/java/example01/PaymentClient.java
@@ -1,33 +1,12 @@
 package com.fanjavaid.strategy_pattern.java.example01;
 
-import com.fanjavaid.strategy_pattern.java.example01.payments.DanaPay;
-import com.fanjavaid.strategy_pattern.java.example01.payments.VirtualAccountPay;
 import com.fanjavaid.strategy_pattern.java.example01.payments.WalletPay;
+import com.fanjavaid.strategy_pattern.java.example01.payments.behavior.IPayBehavior;
 
 public class PaymentClient {
     public static void main(String[] args) {
-//        ITopupBehavior topupBehavior = new WalletTopup();
-//        IPayBehavior payBehavior = new WalletPay();
-
-        // wallet, dana, va
-        String selectedPayment = "va";
-
-        Payment payment = new Payment();
-        switch (selectedPayment) {
-            case "wallet":
-                payment.setPayBehavior(new WalletPay());
-                break;
-
-            case "dana":
-                payment.setPayBehavior(new DanaPay());
-                break;
-
-            case "va":
-                payment.setPayBehavior(new VirtualAccountPay());
-                break;
-        }
-
-//        payment.topup(1000L);
+        IPayBehavior walletBehavior = new WalletPay();
+        Payment payment = new Payment(walletBehavior);
         payment.pay();
     }
 }
